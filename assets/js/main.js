1aa195ad2556d337132c08d7c3175bfc6b78c13f
Maxime <maxime@quixiz.com>
2024-08-03 09:21:47 +0900

    Correction overflow sur mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,4 @@ $(function() {
     $(".mobile-menu-toggle").click(function() {
         $(".mobile-menu-container").toggleClass("active");
     });
-
-    resetHeight();
-    $(window).resize(function() {
-        resetHeight();
-    });
 })
-
-function resetHeight() {
-    var height = $(window).height();
-    $("main").css("height", height);
-}
